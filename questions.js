f5f67696107b7ee7b014f82ea1ffbea2c0e375a8
Tendi <pastendisherpa26@gmail.com>
2023-03-14 19:33:53 +0545

    quiz-questions and js script

---
+++ questions.js
@@ -0,0 +1,111 @@
+const questions = [
+  {
+    numb: 1,
+    question: 'What is React?',
+    answer: 'A JavaScript library for building user interfaces',
+    options: [
+      'A programming language',
+      'A markup language',
+      'A JavaScript library for building user interfaces',
+      'A database management system',
+    ],
+  },
+  {
+    numb: 2,
+    question: 'What is JSX?',
+    answer: 'A syntax extension for JavaScript',
+    options: [
+      'A JavaScript framework',
+      'A syntax extension for JavaScript',
+      'A CSS preprocessor',
+      'A templating engine',
+    ],
+  },
+  {
+    numb: 3,
+    question: 'What is the virtual DOM?',
+    answer: 'A lightweight copy of the actual DOM',
+    options: [
+      'A virtual server that hosts React applications',
+      'A lightweight copy of the actual DOM',
+      'A JavaScript library for managing state',
+      'A programming language used for React development',
+    ],
+  },
+  {
+    numb: 4,
+    question: 'What is a component in React?',
+    answer: 'A reusable piece of code that returns a React element',
+    options: [
+      'A standalone JavaScript function',
+      'A JavaScript object that holds data',
+      'A pre-built UI element',
+      'A reusable piece of code that returns a React element',
+    ],
+  },
+  {
+    numb: 5,
+    question: 'What is props in React?',
+    answer:
+      'Short for "properties", props are inputs passed to a React component',
+    options: [
+      'A reserved keyword in React',
+      'A method for updating state',
+      'Short for "properties", props are inputs passed to a React component',
+      'A method for rendering a component',
+    ],
+  },
+  {
+    numb: 6,
+    question: 'What is the purpose of the render method in React?',
+    answer:
+      'To return a tree of React components that will eventually be rendered to the DOM.',
+    options: [
+      'To create a new component instance.',
+      'To update the component state.',
+      'To render static HTML to the DOM.',
+      'To return a tree of React components that will eventually be rendered to the DOM.',
+    ],
+  },
+  {
+    numb: 7,
+    question: 'Which of the following is true about key props ?',
+    answer:
+      'used identify the newly added elementTo update the state of a component and trigger a re-render.',
+    options: [
+      'used to make prop more identifiable',
+      'used identify the newly added elementTo update the state of a component and trigger a re-render.',
+      'type of attribute of HTML',
+      'not used in the array',
+    ],
+  },
+  {
+    numb: 8,
+    question:
+      'Which below function in React.js is used to get the DOM node when the component is rendered ?',
+    answer: 'findDOMNode()',
+    options: [
+      'renderDOM()',
+      'componentAtNode(',
+      'getDOMNode()',
+      'findDOMNode()',
+    ],
+  },
+  {
+    numb: 9,
+    question: 'What does JSX stand for?',
+    answer: 'JavaScript XML',
+    options: ['JavaScript XML', 'JavaScript XSL', 'Java XML', 'Java XSL'],
+  },
+  {
+    numb: 10,
+    question: 'What is the purpose of React?',
+    answer: 'To build reusable UI components',
+    options: [
+      'To build web applications',
+      'To build mobile applications',
+      'To build reusable UI components',
+      'To build server-side applications',
+    ],
+  },
+]
